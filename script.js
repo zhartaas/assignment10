30b6bd294c0a816dd22a643e38a725ace0a0f3ac
zhartaas <satibaevzhartas@gmail.com>
2023-11-23 17:34:11 +0600

    Little changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let arr = ["apple", "pineapple", "cucumber"];
 
-console.log("This is an array", arr);
+console.log("This is an array of fruits", arr);
 
 for (let i = 0; i < arr.length; i++) {
     console.log(i, ":", arr[i]);
