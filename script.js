c35ea693e5c64b45749a4ae7133cdbd104ae963b
Timofey <han.man.80@bk.ru>
2023-11-23 17:31:26 +0600

    Little changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let arr = ["apple", "pineapple", "cucumber"];
+let arr = ["apple", "pineapple", "cucumber", ];
 
-console.log("This is a array", arr);
+console.log("This is a array of vegetables and fruits", arr);
 
 for (let i = 0; i < arr.length; i++) {
+    console.log(i);
     console.log(arr[i]);
 }
